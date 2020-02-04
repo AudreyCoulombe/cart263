bf0d0a80f2e42fb17eab11638a158db4407c84cf
AudreyCoulombe <coulombe.audrey@example.com>
2020-02-04 11:26:34 -0500

    P1: Changed opacity in animation and displayed the number of sorted elements. Added more objects to sort.

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -11,6 +11,11 @@ A game where you have to sort garbage, recycle and compost elements by dragging 
 // JQuery variable:
 let $objectsToSort;
 
+//java scricpt variable:
+let garbageCount=0;
+let recycleCount=0;
+let compostCount=0;
+
 // Sound variables:
 // let chewingSound = new Audio("assets/sounds/crunch.wav");
 // let buzzSound = new Audio("assets/sounds/buzz.mp3");
@@ -40,6 +45,28 @@ function setup() {
   $('.garbage').droppable({
     // Accept only if the object has a garbage id
     accept: "#garbage",
+    // out: function(event,ui) {
+    // $(this).effect( "shake");
+    // },
+    // accept: "#garbage", function( event, ui ) {
+    //   if ($(this).attr("id") === 'garbage') {
+    //     $('#garbage').effect( "shake");
+    //     return true;
+    //     console.log("match");
+    //   }
+    //   else {
+    //     $('#garbage').effect( "shake");
+    //     return false
+    //     $(this).effect( "shake");
+    //     console.log("no match");
+    //   }
+      // if ($(this).attr("id") === 'garbage') {
+      //   return false;
+      // }
+      // else {
+      //   $(this).effect( "shake");
+      // }
+    // },
     // When dropped, execute the onDrop function
     drop: onDrop
   });
@@ -65,11 +92,11 @@ function setup() {
 // Positions the objects randomly in the objects container
 function positionObjects(index, value) {
   // Create a random number for the height position
-  let objectsHeightPosition = Math.random() * 200;
+  let objectsHeightPosition = Math.random() * 150;
   // Add the height position to the css of the object
-  $(this).css('top', objectsHeightPosition + 'px');
+  $(this).css('bottom', objectsHeightPosition + 'px');
   // Create a random number for the width position
-  let objectsWidthPosition = Math.random() * 300;
+  let objectsWidthPosition = Math.random() * 220;
   // Add the width position to the css of the object
   $(this).css('left', objectsWidthPosition + 'px');
   // console.log("height position: " + objectsHeightPosition);
@@ -83,17 +110,25 @@ function onDrop(event, ui) {
   // if the draggable object has a garbage id...
   if ($(ui.draggable[0]).attr("id") === 'garbage') {
     console.log("garbage dropped");
-    // Animate it in three sequence so it goes back in the container
+    // Add 1 to the garbage count
+    garbageCount += 1;
+    // Display the garbage count as text
+    $('#garbageCount').text(garbageCount);
+    // Animate the object in four sequences so it goes back in the container and reduces opacity while moving
     ui.draggable
       .animate({
-        top: "+=70",
+        opacity: [ 0.25, "linear" ],
+        top: ["+=70", "swing"],
       }, 1000)
       .animate({
-        left: "+=90",
-        top: "+=70",
+        left: ["+=90", "swing"],
+        top: ["+=70", "swing"],
       }, 1000)
       .animate({
-        top: "+=200",
+        top: ["+=200", "swing"],
+      }, 1000)
+      .animate({
+        opacity: [ 1.0, "linear" ]
         //height: "toggle"
       }, 1000, function() {
         // Animation complete.
@@ -102,29 +137,47 @@ function onDrop(event, ui) {
   // if the draggable object has a recycle id...
   else if ($(ui.draggable[0]).attr("id") === 'recycle') {
     console.log("recycle dropped");
-    // Animate it so it goes back in the container
+    // Add 1 to the recycle count
+    recycleCount += 1;
+    // Display the recycle count as text
+    $('#recycleCount').text(recycleCount);
+    // Animate the object in three sequences so it goes back in the container and reduces opacity while moving
     ui.draggable
       .animate({
-        top: "+=400",
-      }, 3000, function() {
+        opacity: [ 0.25, "linear" ],
+        top: ["+=70", "swing"],
+      }, 1000)
+      .animate({
+        top: ["+=200", "swing"],
+      }, 1000)
+      .animate({
+        opacity: [ 1.0, "linear" ]
+      }, 1000, function() {
         // Animation complete.
       });
   }
   // if the draggable object has a compost id...
   else {
     console.log("compost dropped");
+    // Add 1 to the compost count
+    compostCount += 1;
+    // Display the compost count as text
+    $('#compostCount').text(compostCount);
+    // Animate the object in four sequences so it goes back in the container and reduces opacity while moving
     ui.draggable
-      // Animate it in three sequence so it goes back in the container
       .animate({
-        top: "+=70",
+        opacity: [ 0.25, "linear" ],
+        top: ["+=70", "swing"],
       }, 1000)
       .animate({
-        left: "-=90",
-        top: "+=70",
+        left: ["-=90", "swing"],
+        top: ["+=70", "swing"],
       }, 1000)
       .animate({
-        top: "+=200",
-        //height: "toggle"
+        top: ["+=200", "swing"],
+      }, 1000)
+      .animate({
+        opacity: [ 1.0, "linear" ]
       }, 1000, function() {
         // Animation complete.
       });
