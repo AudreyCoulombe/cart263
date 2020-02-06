f803d844e43319204a9edc1b159de36b8c3b6670
AudreyCoulombe <coulombe.audrey@example.com>
2020-02-06 17:32:24 -0500

    P1: Added a sound and shake effect that plays when the item is dropped outside a droppable. Added the read me file and cleaned my code.

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -7,94 +7,97 @@ Audrey Coulombe
 
 A game where you have to sort garbage, recycle and compost elements by dragging them in the rigth bin, but it keeps going back to the initial container with garbage, recycle and compost all mixed up.
 
+* I looked at this example to understand how to use ui.helper.data: https://stackoverflow.com/questions/8092771/jquery-drag-and-drop-checking-for-a-drop-outside-a-droppable
+
 *********************************************************************/
 // JQuery variable:
 let $objectsToSort;
 
 //java scricpt variable:
-let garbageCount=0;
-let recycleCount=0;
-let compostCount=0;
+let garbageCount = 0;
+let recycleCount = 0;
+let compostCount = 0;
 
 // Sound variables:
 let droppingSound = new Audio("assets/sounds/drop.wav");
+let errorSound = new Audio("assets/sounds/errorAlert.wav");
 let gameMusic = new Audio("assets/sounds/gameMusic.mp3");
 
-// Runs setup only once the page Document Object Model is ready
+// Run setup when the page is ready
 $(document).ready(setup);
 
 // setup()
 //
-//
+// Displays a dialog box, calls the positionObjects function, makes the objects to sort draggable and the 3 sections of the bin droppable.
 function setup() {
   // Open a dialog box before the game starts
-  $( '.startBox' ).dialog({
-    modal: true,
-    resizable: false,
-    draggable: false,
-    dialogClass: "dialogStyle",
-    height: 400,
+  $('.startBox').dialog({
+    modal: true, // Blur the background
+    resizable: false, // Disable resizable option
+    draggable: false, // Disable draggable option
+    dialogClass: "dialogStyle", // Add a class
+    height: 400, // Set dimensions
     width: 600,
-    autoOpen: true,
+    autoOpen: true, // Open the dialog box automatically
     buttons: [{
       text: "Click here to start",
+      // When we click the button...
       click: function() {
-        $( this ).dialog( "destroy" );
+        // Destroy the dialog box,
+        $(this).dialog("destroy");
+        // Replace the text in the div with no text
         $('.startBox').text("");
-        playMusic();
+        // And start playing the music in loop
+        gameMusic.play();
+        gameMusic.loop = true;
       }
     }],
   });
 
-  // Put all objects with the specific class in a variable
+  // Put all objects with the "objectsToSort" class in a variable
   $objectsToSort = $('.objectsToSort');
-
   // For each object in the "array", execute the positionObjects function
   $objectsToSort.each(positionObjects);
-
   // Make the objects to sort draggable
   $objectsToSort.draggable({
-    revert: "invalid",
-    // When you begin to drag, play a sound
-    start: function() {
-      // buzzSound.play();
+    // When you begin to drag, set dropped to false*
+    start: function(event, ui) {
+      ui.helper.data('dropped', false);
     },
-    // When you stop dragging, play another sound
-    stop: function() {
-      droppingSound.play();
-      // droppingSound.pause();
-    }
+    // When you stop dragging...
+    stop: function(event, ui) {
+      // If the object is not dropped on a droppable*...
+      if (ui.helper.data('dropped') === false) {
+        // Play the error sound
+        errorSound.play();
+        // And shake the object
+        $(this).effect("shake");
+      }
+    },
+    // Revert the object when it is dropped outside a droppable
+    revert: "invalid",
+    revertDuration: 200
   });
 
-  // Make the objects with the class garbage droppable
+  // Make the div with the class garbage droppable
   $('.garbage').droppable({
-    // Accept only if the object has a garbage id
+    // Accept only if the dropped object has a garbage id
     accept: "#garbage",
-    // accept: function (ui,event) {
-    //     if ($(ui.droppable[0]).attr("id") === 'garbage') {
-    //       return true;
-    //     }
-    //     else {
-    //       return false;
-    //       $(ui.droppable[0]).effect( "shake");
-    //     }
-    //   },
     // When dropped, execute the onDrop function
-    drop: onDrop,
+    drop: onDrop
   });
-  console.log($('.garbage').droppable( "option" ));
 
-  // Make the objects with the class recycle droppable
+  // Make the div with the class recycle droppable
   $('.recycle').droppable({
-    // Accept only if the object has a recycle id
+    // Accept only if the dropped object has a recycle id
     accept: "#recycle",
     // When dropped, execute the onDrop function
     drop: onDrop
   });
 
-  // Make the objects with the class compost droppable
+  // Make the div with the class compost droppable
   $('.compost').droppable({
-    // Accept only if the object has a compost id
+    // Accept only if the dropped object has a compost id
     accept: "#compost",
     // When dropped, execute the onDrop function
     drop: onDrop
@@ -103,7 +106,7 @@ function setup() {
 
 // positionObjects()
 //
-// Positions the objects randomly in the objects container
+// Positions and rotates the objects randomly in the objects container
 function positionObjects(index, value) {
   // Create a random number for the height position
   let objectsHeightPosition = Math.random() * 150 + 10;
@@ -116,17 +119,21 @@ function positionObjects(index, value) {
   // Rotate the object with a random angle (so it looks more like real garbage)
   let rotationAngle = Math.random() * 360;
   // Add the rotation value to the css of the object
-  $(this).css({'transform' : 'rotate(' + rotationAngle + 'deg)'});
+  $(this).css({
+    'transform': 'rotate(' + rotationAngle + 'deg)'
+  });
 }
 
 // onDrop()
 //
-// Animates the object when dropped
+// When dropped, play the a sound and animate the object
 function onDrop(event, ui) {
-  console.log(ui.draggable[0]);
-  // if the draggable object has a garbage id...
+  // Set dropped to true
+  ui.helper.data('dropped', true);
+  // Play the dropping sound
+  droppingSound.play();
+  // If the draggable object has a garbage id...
   if ($(ui.draggable[0]).attr("id") === 'garbage') {
-    console.log("garbage dropped");
     // Add 1 to the garbage count
     garbageCount += 1;
     // Display the garbage count as text
@@ -134,8 +141,8 @@ function onDrop(event, ui) {
     // Animate the object in four sequences so it goes back in the container and reduces opacity while moving
     ui.draggable
       .animate({
-        opacity: [ 0.25, "linear" ],
-        top: ["+=70", "swing"],
+        opacity: [0.25, "linear"],
+        top: ["+=50", "swing"],
       }, 1000)
       .animate({
         left: ["+=110", "swing"],
@@ -145,15 +152,11 @@ function onDrop(event, ui) {
         top: ["+=250", "swing"],
       }, 1000)
       .animate({
-        opacity: [ 1.0, "linear" ]
-        //height: "toggle"
-      }, 1000, function() {
-        // Animation complete.
-      });
+        opacity: [1.0, "linear"],
+      }, 1000);
   }
-  // if the draggable object has a recycle id...
+  // If the draggable object has a recycle id...
   else if ($(ui.draggable[0]).attr("id") === 'recycle') {
-    console.log("recycle dropped");
     // Add 1 to the recycle count
     recycleCount += 1;
     // Display the recycle count as text
@@ -161,21 +164,18 @@ function onDrop(event, ui) {
     // Animate the object in three sequences so it goes back in the container and reduces opacity while moving
     ui.draggable
       .animate({
-        opacity: [ 0.25, "linear" ],
-        top: ["+=70", "swing"],
+        opacity: [0.25, "linear"],
+        top: ["+=50", "swing"],
       }, 1000)
       .animate({
         top: ["+=300", "swing"],
       }, 1000)
       .animate({
-        opacity: [ 1.0, "linear" ]
-      }, 1000, function() {
-        // Animation complete.
-      });
+        opacity: [1.0, "linear"]
+      }, 1000);
   }
   // if the draggable object has a compost id...
   else {
-    console.log("compost dropped");
     // Add 1 to the compost count
     compostCount += 1;
     // Display the compost count as text
@@ -183,8 +183,8 @@ function onDrop(event, ui) {
     // Animate the object in four sequences so it goes back in the container and reduces opacity while moving
     ui.draggable
       .animate({
-        opacity: [ 0.25, "linear" ],
-        top: ["+=70", "swing"],
+        opacity: [0.25, "linear"],
+        top: ["+=50", "swing"],
       }, 1000)
       .animate({
         left: ["-=110", "swing"],
@@ -194,17 +194,7 @@ function onDrop(event, ui) {
         top: ["+=250", "swing"],
       }, 1000)
       .animate({
-        opacity: [ 1.0, "linear" ]
-      }, 1000, function() {
-        // Animation complete.
-      });
+        opacity: [1.0, "linear"]
+      }, 1000);
   }
-}
-
-// playMusic()
-//
-function playMusic(){
-  // Play a sound in loop
-  gameMusic.play();
-  gameMusic.loop = true;
 }
