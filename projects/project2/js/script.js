3776f9e908051627163cc123dde06a3ea228472d
AudreyCoulombe <coulombe.audrey@example.com>
2020-02-27 14:14:54 -0500

    P2: After a talk with Sabine, decided to switch from P5 to jQuery library and put my images directly in index.html instead of in script.js. Deleted a lot of things that is no more relevant because of the switch of library and because I changed the way I display images

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -19,120 +19,40 @@ let spinningLegsInterval;
 let spinningBodiesInterval;
 let spinningHeadsInterval;
 
-// Background image variable
-let backgroundImage;
-// Heads images
-let headDora;
-let headElmo;
-let headMomo;
-let headPeppaPig;
-let headPo;
-// Body images
-let bodyDora;
-let bodyElmo;
-let bodyMinion;
-let bodyPeppaPig;
-let bodyWarrior;
-// Legs images
-let legDora;
-let legElmo;
-let legMickey;
-let legPeggaPig;
-let legSexy;
 
-// Array with all the body body parts
-let bodyParts = [];
-// Position of the body parts
-let bodyPartsX = 0;
-let bodyPartsY = 150;
-// Width and height of the body parts
-let bodyPartsWidth = 440;
-let bodyPartsHeight = 375;
-
-// preload()
-//
-// Preloads images and put the body parts in an array
-function preload() {
-  // Load the background image
-  backgroundImage = loadImage("assets/images/backgroundLandscape.png");
-  // Load all the legs images and push them in the array with the body parts
-  legDora = loadImage("assets/images/legDora.png");
-  bodyParts.push(legDora);
-  legElmo = loadImage("assets/images/legElmo.png");
-  bodyParts.push(legElmo);
-  legMickey = loadImage("assets/images/legMickey.png");
-  bodyParts.push(legMickey);
-  legPeggaPig = loadImage("assets/images/legPeggaPig.png");
-  bodyParts.push(legPeggaPig);
-  legSexy = loadImage("assets/images/legSexy.png");
-  bodyParts.push(legSexy);
-  // Load all the body images and push them in the array with the body parts
-  bodyDora = loadImage("assets/images/bodyDora.png");
-  bodyParts.push(bodyDora);
-  bodyElmo = loadImage("assets/images/bodyElmo.png");
-  bodyParts.push(bodyElmo);
-  bodyMinion = loadImage("assets/images/bodyMinion.png");
-  bodyParts.push(bodyMinion);
-  bodyPeppaPig = loadImage("assets/images/bodyPeppaPig.png");
-  bodyParts.push(bodyPeppaPig);
-  bodyWarrior = loadImage("assets/images/bodyWarrior.png");
-  bodyParts.push(bodyWarrior);
-  // Load all the head images and push them in the array with the body parts
-  headDora = loadImage("assets/images/headDora.png");
-  bodyParts.push(headDora);
-  headElmo = loadImage("assets/images/headElmo.png");
-  bodyParts.push(headElmo);
-  headMomo = loadImage("assets/images/headMomo.png");
-  bodyParts.push(headMomo);
-  headPeppaPig = loadImage("assets/images/headPeppaPig.png");
-  bodyParts.push(headPeppaPig);
-  headPo = loadImage("assets/images/headPo.png");
-  bodyParts.push(headPo);
-}
+$(document).ready(setup);
 
 // setup()
 //
 // Creates Canvas and displays title page
 function setup() {
-  // Creates the canvas
-  createCanvas(1400, 750);
-  // Displays background image as title image
-  image(backgroundImage, 0, 0, width, height);
-  // And displays text over it
-  text("Title page", width/2, height/2);
-}
-
-// draw()
-//
-// Description of draw()
-function draw() {
-
+  displayButtons();
 }
 
 //  mousePressed()
 //
 // When the mouse is pressed, checks if we are at the state TITLE.
 // If so, displays the "game" arrangement
-function mousePressed() {
-  // If the state is "TITLE"...
-  if (state === "TITLE") {
-    // Display the background image
-    image(backgroundImage, 0, 0, width, height);
-    // And the buttons
-    displayButtons();
-    // And change the state to "GAME"
-    state = "GAME";
-  }
-}
+// function mousePressed() {
+//   // If the state is "TITLE"...
+//   if (state === "TITLE") {
+//     // Display the background image
+//     image(backgroundImage, 0, 0, width, height);
+//     // And the buttons
+//     displayButtons();
+//     // And change the state to "GAME"
+//     state = "GAME";
+//   }
+// }
 
 // displayButtons()
 //
 // Changes the css of the buttons so we can see it
 function displayButtons() {
   // Stocking the 3 buttons in 3 variables
-  let headsButton = document.getElementById("heads");
-  let bodiesButton = document.getElementById("bodies");
-  let legsButton = document.getElementById("legs");
+  let headsButton = document.getElementById("headsButton");
+  let bodiesButton = document.getElementById("bodiesButton");
+  let legsButton = document.getElementById("legsButton");
   // And change their display mode in css to "block"
   headsButton.style.display = "block";
   bodiesButton.style.display = "block";
@@ -142,23 +62,31 @@ function displayButtons() {
 // spinLegsAndStop()
 //
 // Checks is the legs are already spinning or not
-// Spins it when not spinning and vice versa
-function spinLegsAndStop() {
-  // If the the legs are not spinning
-  if(legsSpinning === false){
-    // Set an interval and store it in a variable
-    spinningLegsInterval = setInterval(function(){
-      // Display the next images by the top left corner
-      imageMode(CORNER);
-      // Display the background image over the previous one
-      image(backgroundImage, 0, 0, width, height);
-      imageMode(CENTER);
-      image(bodyParts[Math.floor(Math.random()*5)], width/2, height/2, bodyPartsWidth, bodyPartsHeight);
-    }, 100);
-    legsSpinning = true;
-  }
-  else if (legsSpinning===true){
-    clearInterval(spinningLegsInterval);
-    legsSpinning = false;
-  }
-}
+// Spins it when not spinning and vice versa by randomly displaying legs images in the array at an interval of 100 miliseconds
+// function spinLegsAndStop() {
+//   // If the the legs are not spinning yet
+//   if(legsSpinning === false){
+//     // Set an interval and store it in a variable
+//     spinningLegsInterval = setInterval(function(){
+//       // Display the next images by the top left corner
+//       imageMode(CORNER);
+//       // Display the background image over the previous one
+//       image(backgroundImage, 0, 0, width, height);
+//       // Display the next images by center
+//       imageMode(CENTER);
+//       // Display one of the 5 first body part image (the 5 legs images) randomly
+//       image(bodyParts[Math.floor(Math.random()*5)], width/2, height/2, bodyPartsWidth, bodyPartsHeight);
+//       }
+//     // Set the interval of repetition of the previous function to 100 miliseconds
+//     , 100);
+//     // And change the state of the legs spinning to true
+//     legsSpinning = true;
+//   }
+//   // (When the button is clicked) if the legs are already moving...
+//   else if (legsSpinning===true){
+//     // Stop the interval so the image stays visible
+//     clearInterval(spinningLegsInterval);
+//     // And change the state of the legs spinning to false
+//     legsSpinning = false;
+//   }
+// }
