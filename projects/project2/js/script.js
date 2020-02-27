98c30d5216c09d70f77955106ba46a0a6ee4d1ec
AudreyCoulombe <coulombe.audrey@example.com>
2020-02-27 15:29:16 -0500

    P2: Switched the order of the head, body, and legs images so the legs are in the back and the heads on front. Since I deleted the start page for now, made the buttons visibles and deleted the display buttons function. Changed the way images are displayed in the function spinLegsAndStop(): now uses jquerry hide() and show(). Copied this function and changed variable for spinBodiesAndStop();

--- projects/project2/js/script.js
+++ projects/project2/js/script.js
@@ -9,7 +9,6 @@ A game where you have to generate kids caracters and titles as quick as possible
 
 ******************/
 // Initial states of the game
-let state = "TITLE";
 let legsSpinning = false;
 let bodiesSpinning = false;
 let headsSpinning = false;
@@ -26,7 +25,7 @@ $(document).ready(setup);
 //
 // Creates Canvas and displays title page
 function setup() {
-  displayButtons();
+  $('.bodyPartsImages').hide();
 }
 
 //  mousePressed()
@@ -45,48 +44,58 @@ function setup() {
 //   }
 // }
 
-// displayButtons()
-//
-// Changes the css of the buttons so we can see it
-function displayButtons() {
-  // Stocking the 3 buttons in 3 variables
-  let headsButton = document.getElementById("headsButton");
-  let bodiesButton = document.getElementById("bodiesButton");
-  let legsButton = document.getElementById("legsButton");
-  // And change their display mode in css to "block"
-  headsButton.style.display = "block";
-  bodiesButton.style.display = "block";
-  legsButton.style.display = "block";
-}
-
 // spinLegsAndStop()
 //
-// Checks is the legs are already spinning or not
+// Checks if the legs are already spinning or not
 // Spins it when not spinning and vice versa by randomly displaying legs images in the array at an interval of 100 miliseconds
-// function spinLegsAndStop() {
-//   // If the the legs are not spinning yet
-//   if(legsSpinning === false){
-//     // Set an interval and store it in a variable
-//     spinningLegsInterval = setInterval(function(){
-//       // Display the next images by the top left corner
-//       imageMode(CORNER);
-//       // Display the background image over the previous one
-//       image(backgroundImage, 0, 0, width, height);
-//       // Display the next images by center
-//       imageMode(CENTER);
-//       // Display one of the 5 first body part image (the 5 legs images) randomly
-//       image(bodyParts[Math.floor(Math.random()*5)], width/2, height/2, bodyPartsWidth, bodyPartsHeight);
-//       }
-//     // Set the interval of repetition of the previous function to 100 miliseconds
-//     , 100);
-//     // And change the state of the legs spinning to true
-//     legsSpinning = true;
-//   }
-//   // (When the button is clicked) if the legs are already moving...
-//   else if (legsSpinning===true){
-//     // Stop the interval so the image stays visible
-//     clearInterval(spinningLegsInterval);
-//     // And change the state of the legs spinning to false
-//     legsSpinning = false;
-//   }
-// }
+function spinLegsAndStop() {
+  // If the the legs are not spinning yet
+  if(legsSpinning === false){
+    // Set an interval and store it in a variable
+    spinningLegsInterval = setInterval(function(){
+      let legs = $('.leg');
+      legs.hide();
+      let leg = legs[Math.floor(Math.random()*legs.length)];
+      $(leg).show();
+    }
+    // Set the interval of repetition of the previous function to 100 miliseconds
+    , 100);
+    // And change the state of the legs spinning to true
+    legsSpinning = true;
+  }
+  // (When the button is clicked) if the legs are already moving...
+  else if (legsSpinning===true){
+    // Stop the interval so the image stays visible
+    clearInterval(spinningLegsInterval);
+    // And change the state of the legs spinning to false
+    legsSpinning = false;
+  }
+}
+
+// spinBodiesAndStop()
+//
+// Checks if the bodies are already spinning or not
+// Spins it when not spinning and vice versa by randomly displaying legs images in the array at an interval of 100 miliseconds
+function spinBodiesAndStop() {
+  // If the the bodies are not spinning yet
+  if(bodiesSpinning === false){
+    // Set an interval and store it in a variable
+    spinningBodiesInterval = setInterval(function(){
+      let bodies = $('.body');
+      bodies.hide();
+      let body = bodies[Math.floor(Math.random()*bodies.length)];
+      $(body).show();
+    }
+    // Set the interval of repetition of the previous function to 100 miliseconds
+    , 100);
+    // And change the state of the bodies spinning to true
+    bodiesSpinning = true;
+  }
+  // (When the button is clicked) if the bodies are already moving...
+  else if (bodiesSpinning===true){
+    // Stop the interval so the image stays visible
+    clearInterval(spinningBodiesInterval);
+    // And change the state of the bodies spinning to false
+    bodiesSpinning = false;
+  }
+}
