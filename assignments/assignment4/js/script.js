a0a6e0186c0fc9954d65d6ea72d4f75a4fcfcd8a
AudreyCoulombe <coulombe.audrey@example.com>
2020-03-28 17:50:24 -0400

    A4: Added an event handler in gotData function that calls that same function when we click on the body.  Changed the way of displaying the sentence using .html() instead of .append() so the sentence will be replaced rather than added.

--- assignments/assignment4/js/script.js
+++ assignments/assignment4/js/script.js
@@ -1,7 +1,7 @@
 /*
 
 Condiments
-Pippin Barr
+Audrey Coulombe
 
 Chooses random words from local JSON data to fill out a sentence
 describing a condiment based on cats and rooms... weird.
@@ -13,8 +13,8 @@ https://github.com/dariusk/corpora
 
 */
 
+// When the page is ready, load the data
 $(document).ready(function() {
-
   // The first thing we need to do is load the data we're going
   // to use to get random words.
   //
@@ -22,7 +22,9 @@ $(document).ready(function() {
   // the location of the file, and a function to call when the data
   // is available...
   $.getJSON('data/data.json')
+    // When the data is done loading, execute the function gotData()
     .done(gotData)
+    // If there is an error loading the data, execute the function dataError()
     .fail(dataError);
 });
 
@@ -31,9 +33,7 @@ $(document).ready(function() {
 // This function gets called by getJSON when the data has been loaded.
 // The data itself will be in the 'data' argument as a JavaScript object.
 function gotData(data) {
-  // Now we select random elements from the three arrays inside
-  // our JSON to get a random condiment, cat, and room. Then we add those
-  // words onto our page by setting the text of the appropriate span.
+  // Now we select random elements from the arrays inside our JSON to get a random condiment, color, cat, room and board game. Then we add those words into the paragraph with the id "sentence" by setting the text of the appropriate span.
 
   // First the condiment
   // Get a random condiment from the condiments array in the JSON
@@ -76,15 +76,21 @@ function gotData(data) {
   }
 
   // Get a random element in the games data
-  let boardGame = getRandomElement(data.games)
+  let boardGame = getRandomElement(data.games);
 
   // Now we can construct our description with a template string
   // We have the basic structure of a sentence and we substitute in the
   // values we've just calculated
   let description = `${condiment} ${verb} like ${indefiniteArticleColor} ${colorName} ${cat} in ${indefiniteArticleRoom} ${room} playing ${boardGame}.`;
 
-  // Finally, we add it to the page and hey presto!
-  $('body').append(description)
+  // Finally, we add it to the paragraph with the id "sentence" and hey presto!
+  $("#sentence").html(description);
+
+  // When we click on the body, execute the function gotData()
+  $('body').on('click', function() {
+    // (I put the function in an anonymous function to be able to call the argument)
+    gotData(data);
+  });
 }
 
 // dataError()
