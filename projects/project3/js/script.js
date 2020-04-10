a5969b7706f01c565726da227fe66e7e004e998c
AudreyCoulombe <coulombe.audrey@example.com>
2020-04-10 15:30:20 -0400

    P3: Added a toilet paper roll image and displayed 8 of it randomly in the page. Made a function displayElement() that I can reuse to display the characters and the toilet paper rolls. Added a function displayPaperRolls() that calls the function displayElement().

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -15,6 +15,7 @@ Used the following link as a reference for the walk pattern: https://digipiph.co
 //
 // Initial states of the game
 let playerMoving = false;
+let playerCollision = false;
 // Position variables
 let playerPosition; // Note to myself: I can access playerPosition.left and playerPosition.top
 let mousePosition = {
@@ -30,15 +31,13 @@ let playerWalkFrame = 0;
 // Player velocity variables
 let playerVelocityX;
 let playerVelocityY;
-// Player rotation angle (in radians)
-let playerRotation;
+// Number of characters other than the player and number of toilet paper rolls
 let numberOfCharacters = 8;
-// Array that contains all the characters divs (other than the player)
-let characters = [];
+let numberOfPaperRoll = 8;
 // Variable that contains the player character div
 let $playerCharacter;
-let playerCollision = false;
-let characterCollision;
+// Variable in which we will store like in an array the elements touched by the player
+let playerTouched;
 
 // When the page is ready...
 $(document).ready(function() {
@@ -46,12 +45,15 @@ $(document).ready(function() {
   $playerCharacter = $('#player');
   // Display the characters other than the player randomly in the body
   displayCharacters();
+  // Display the toilet paper rolls randomly in the body
+  displayPaperRolls();
   // Check the mouse and player positions each millisecond
   setInterval(mousePlayerPosition,1);
-  // M
+  // Move all charcters other than the player and change the velocity each 2 seconds
   moveCharacters();
-  // And check if it's time to walk and if so, anime the walk pattern
+  // Check if it's time to walk and if so, anime the walk pattern
   setInterval(playerWalk, walkingSpeed);
+  // Check if the player touched something each 60 millisecond
   setInterval(checkPlayerCollision,60);
 });
 
@@ -61,10 +63,9 @@ $(document).ready(function() {
 // If so, animes the player with the "explode" effect...
 function checkPlayerCollision() {
   // When the player touches a character, store the character object in an "array", in a variable
-  characterCollision = $($playerCharacter).collision('.character'); // function from the library extension "jquery-collision"
-  // console.log(characterCollision);
-  // If the characterCollision "array" contain more than 0 object...
-  if (characterCollision.length > 0) {
+  playerTouched = $($playerCharacter).collision('.character'); // function from the library extension "jquery-collision"
+  // If the playerTouched "array" contain more than 0 object...
+  if (playerTouched.length > 0) {
     // Set the player collision state to true;
     playerCollision = true;
     // Anime the player with the "explode" effect and when the animation is done...
@@ -77,34 +78,56 @@ function checkPlayerCollision() {
       $playerCharacter.show();
     } );
   }
-  // console.log(playerCollision);
-  // console.log(characterCollision.length);
 }
 
 // displayCharacters()
 //
-// For each character, creates a div with character class,
-// sets the background image as the character image and displays it randomly in the body
+// For each character, creates a new div, displays it randomly, adds the class "character"
+// and sets the background image as the character image
 function displayCharacters() {
   // For each character...
   for (let i = 1; i<numberOfCharacters+1; i++) {
-    // Create a div and store it in a variable
+    // Create a new div and store it in a variable
     let $characterDiv = $("<div></div>");
+    // Set a random position and rotation angle for the new div and display it in the body
+    displayElement($characterDiv);
     // Add the class "character" to the new div
     $characterDiv.addClass("character");
     // Set the background image to the character number we are at
     $characterDiv.css('background-image', 'url(../assets/images/characters-0'+i+'.png)');
-    // Get a random number between 0 and de width of the body for the left position
-    let leftPosition = Math.random() * $('body').width();
-    // Get a random number between 0 and de height of the body for the top position
-    let topPosition = Math.random() * $('body').height();
-    // Set the position of the new div to the random one we just created
-    $characterDiv.offset({ top: topPosition, left: leftPosition });
-    // Add the new div to the body
-    $characterDiv.appendTo($('body'));
-    // And push this div in the array that contains all the characters other than the player
-    characters.push($characterDiv);
   }
+}
+
+// displayPaperRolls()
+//
+// For each paper roll, creates a new div, displays it randomly and adds the class "paperRoll"
+function displayPaperRolls() {
+  // For each character...
+  for (let i = 0; i<numberOfPaperRoll; i++) {
+    // Create a new div and store it in a variable
+    let $paperRollDiv = $("<div></div>");
+    // Set a random position and rotation angle for the new div and display it in the body
+    displayElement($paperRollDiv);
+    // Add the class "paperRoll" to the new div
+    $paperRollDiv.addClass("paperRoll");
+  }
+}
+// displayElement()
+//
+// Gets a random number for the position and rotation angle and displays elements randomly in the body
+// Function used to display the characters other than the player and for toilet paper rolls
+function displayElement($newDiv) {
+  // Get a random number between 0 and de width of the body for the left position
+  let leftPosition = Math.random() * $('body').width();
+  // Get a random number between 0 and de height of the body for the top position
+  let topPosition = Math.random() * $('body').height();
+  // Set the position of the new div to the random one we just created
+  $newDiv.offset({ top: topPosition, left: leftPosition });
+  // gat a random number for the rotation angle in radians
+  let rotation = Math.random() * Math.PI *2;// 2 Pi radians = 360 degrees
+  $newDiv.css({transform: 'rotate('+rotation+'rad)'});
+  // Add the new div to the body
+  $newDiv.appendTo($('body'));
 }
 
 // moveCharacters()
@@ -143,7 +166,7 @@ function moveCharacters() {
         characterWalkFrame = 0;
       }
       // Make the character walk, move and and rotate
-      walk($character, characterVelocityX, characterVelocityY, characterRotation, characterWalkFrame);
+      walk($character, characterVelocityX, characterVelocityY, characterWalkFrame);
       handleWrapping($character);
     },walkingSpeed);
   });
@@ -227,7 +250,7 @@ function playerWalk() {
       playerWalkFrame = 0;
     }
     // Make the player walk, move and rotate
-    walk($playerCharacter, playerVelocityX, playerVelocityY, playerRotation, playerWalkFrame);
+    walk($playerCharacter, playerVelocityX, playerVelocityY, playerWalkFrame);
   }
   // Else, if the player is not moving...
   else {
@@ -239,7 +262,7 @@ function playerWalk() {
 // Walk()
 // Animes the character's sprite sheet, moves the character and rotates it.
 // Function used for the player and the other characters
-function walk($walkingCharacter, velocityX, velocityY, rotationAngle, currentWalkFrame) {
+function walk($walkingCharacter, velocityX, velocityY, currentWalkFrame) {
   // Get the character position and store it un a variable
   let position = $walkingCharacter.offset();
   // Move the character by adding velocity to its top and left position
@@ -247,7 +270,7 @@ function walk($walkingCharacter, velocityX, velocityY, rotationAngle, currentWal
   // Animates the character's background position according to the current walk frame
   $walkingCharacter.css('background-position',walkPattern[currentWalkFrame]);
   // Set the character rotation angle according ti the X & Y velocity (in radians)
-  rotationAngle = Math.atan(velocityY/velocityX) + Math.PI/2;// Math.PI/2 radians === 90 degrees
+  let rotationAngle = Math.atan(velocityY/velocityX) + Math.PI/2;// Math.PI/2 radians === 90 degrees
   // If the distanceX is negative...
   if(velocityX < 0) {
     // Add pi randians to the rotation angle (180 degrees)
